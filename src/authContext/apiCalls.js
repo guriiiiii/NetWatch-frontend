7779fa177ab13800b0d76bfc29286025faa4b476
Guri <gurtegs11@gmail.com>
2024-03-10 23:47:29 -0600

    further changes

--- src/authContext/apiCalls.js
+++ src/authContext/apiCalls.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
+// Create an instance of Axios with custom configuration
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_PROXY_URL // Assuming you've defined this environment variable
+});
+
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("auth/login", user);
+    // Use the axios instance for making requests
+    const res = await axiosInstance.post("auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
